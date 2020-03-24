d39c3706c6c7ac562b2721c068db255ba850b4e9
Nikolay_Volkov <nikolay_volkov@epam.com>
2020-03-24 23:01:12 +0300

    Widget roles for Tabs

--- js/custom.js
+++ js/custom.js
@@ -1,50 +1,155 @@
-(function() {
-  var burger = document.querySelector(".burger");
-  var menu = document.querySelector("#" + burger.dataset.target);
-  var toggleBurger = function () {
-      burger.classList.toggle("is-active");
-      menu.classList.toggle("is-active");
-  };
-  burger.addEventListener("click", function() {
-      toggleBurger()
-  });
+(function () {
+    var burger = document.querySelector(".burger");
+    var menu = document.querySelector("#" + burger.dataset.target);
+    var toggleBurger = function () {
+        burger.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
+    };
+    burger.addEventListener("click", function () {
+        toggleBurger()
+    });
 
-  burger.addEventListener('keypress', function (event) {
-      if (event.key === 'Enter' || event.keyCode === 32) {
-          toggleBurger()
-      }
-  });
+    burger.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter' || event.keyCode === 32) {
+            toggleBurger()
+        }
+    });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
-    toggleTab(this.id, this.dataset.target);
-  };
+document.querySelectorAll("#nav li").forEach(function (navEl) {
+    navEl.onclick = function () {
+        toggleTab(this.id, this.dataset.target);
+    };
 });
 
-function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
 
-  navEls.forEach(function(navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
-    } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
-      }
-    }
-  });
 
-  var tabs = document.querySelectorAll(".tab-pane");
 
-  tabs.forEach(function(tab) {
-    if (tab.id == targetId) {
-      tab.style.display = "block";
-    } else {
-      tab.style.display = "none";
-    }
-  });
+// Listen to tab focus | blur
+function listenTabFocus() {
+    const navEls = document.querySelectorAll("#nav li button");
+    navEls.forEach(function (navEl) {
+        navEl.addEventListener('focus', function () {
+            navEl.addEventListener('keydown', tabKeyDown);
+        });
+        navEl.addEventListener('blur', function () {
+            navEl.removeEventListener('keydown', tabKeyDown);
+        });
+    });
 }
+listenTabFocus();
+
+
+function getActiveTabIndex() {
+    const navEls = document.querySelectorAll("#nav li");
+
+    for (let i = 0; i < navEls.length; i++) {
+        if (navEls[i].classList.contains('is-active')) {
+            return i;
+        }
+    }
+}
+
+function setNextTab() {
+    const navEls = document.querySelectorAll("#nav li");
+    const activeElIndex = getActiveTabIndex();
+    const nextTabIndex = activeElIndex + 1 < navEls.length ? activeElIndex + 1 : 0;
+
+    for (let i = 0; i < navEls.length; i++) {
+        const el = navEls[i]
+        const button = el.querySelector('button');
+
+        if (i === nextTabIndex) {
+            el.classList.add('is-active');
+            button.focus();
+            button.setAttribute('tabindex', '0');
+            button.setAttribute('aria-selected', 'true');
+        } else {
+            if (el.classList.contains('is-active')) {
+                el.classList.remove('is-active');
+                button.setAttribute('tabindex', '-1');
+                button.setAttribute('aria-selected', 'false');
+            }
+        }
+    }
+
+    setActiveTabPane(navEls[nextTabIndex].dataset.target);
+}
+
+function setPrevTab() {
+    const navEls = document.querySelectorAll("#nav li");
+    const activeElIndex = getActiveTabIndex();
+    const nextTabIndex = activeElIndex - 1 >= 0 ? activeElIndex - 1 : navEls.length - 1;
+
+    for (let i = 0; i < navEls.length; i++) {
+        const el = navEls[i]
+        const button = el.querySelector('button');
+
+        if (i === nextTabIndex) {
+            el.classList.add('is-active');
+            button.focus();
+            button.setAttribute('tabindex', '0');
+            button.setAttribute('aria-selected', 'true');
+        } else {
+            if (el.classList.contains('is-active')) {
+                el.classList.remove('is-active');
+                button.setAttribute('tabindex', '-1');
+                button.setAttribute('aria-selected', 'false');
+            }
+        }
+    }
+
+    setActiveTabPane(navEls[nextTabIndex].dataset.target);
+}
+
+function tabKeyDown(event) {
+    switch (event.keyCode) {
+        case 39:
+            setNextTab()
+            break;
+        case 37:
+            setPrevTab()
+            break;
+        default:
+            break;
+    }
+}
+
+function setActiveTabPane(tabId) {
+    var tabs = document.querySelectorAll(".tab-pane");
+
+    tabs.forEach(function (tab) {
+        if (tab.id == tabId) {
+            tab.style.display = "block";
+        } else {
+            tab.style.display = "none";
+        }
+    });
+}
+
+function toggleTab(selectedNav, targetId) {
+    var navEls = document.querySelectorAll("#nav li");
+
+    navEls.forEach(function (navEl) {
+        const button = navEl.querySelector('button');
+        if (navEl.id == selectedNav) {
+            navEl.classList.add("is-active");
+            button.setAttribute('tabindex', "0");
+            button.setAttribute('aria-selected', 'true');
+        } else {
+            if (navEl.classList.contains("is-active")) {
+                navEl.classList.remove("is-active");
+                button.setAttribute('tabindex', "-1")
+                button.setAttribute('aria-selected', 'false');
+            }
+        }
+    });
+    setActiveTabPane(targetId)
+}
+
+
+
+
 
 
 function skipTo() {
@@ -57,7 +162,6 @@ function skipTo() {
     const links = document.querySelectorAll('.skip-to-link');
 
     let activeLinkIndex = -1;
-
 
 
     const setWrapperVisible = function () {
@@ -109,7 +213,7 @@ function skipTo() {
         button.removeEventListener('click', handleButtonClick);
     };
 
-    const handleWrapperFocus = function (){
+    const handleWrapperFocus = function () {
         setWrapperVisible();
     };
 
@@ -137,7 +241,7 @@ function skipTo() {
         }
     };
     const handleWindowClick = function (event) {
-        const { target } = event;
+        const {target} = event;
 
         if (target.classList.contains('is-skip-to') || target.id === 'skip-to__button') {
             return undefined;
